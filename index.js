3a3b2ec3b4e924339a38005576b22fc5a7471023
MadameRocktrice <n7yjfdgs2z@privaterelay.appleid.com>
2026-03-20 20:22:54 +0100

    Port auf 3001 geändert

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 // App erstellen
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
 
 // Middleware (Helfer)
 app.use(cors()); // Frontend darf mit Backend reden
